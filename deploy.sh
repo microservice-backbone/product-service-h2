954ee0ca8c94f935a9b203478aeeb97beeecc901
tansudasli <tansudasli@gmail.com>
2020-05-03 01:34:05 +0300

    minor update

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
-# you need a GKE cluster
-# configure kubectl for your cluster!
+# ./gradlew dockerTag
+# ./gradlew dockerPushGCP
+# create a GKE cluster
+# configure kubectl for your cluster!:  source .env && gcloud container clusters get-credentials $CLUSTER_NAME --region $REGION --project $PROJECT_ID
 
 # create deployment, hpa and service
 kubectl create -f deployment.yaml      #create pods
