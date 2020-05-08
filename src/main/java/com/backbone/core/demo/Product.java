c516806c451a2f695cce2c1ce703ad8fe0cd8208
tansudasli <tansudasli@gmail.com>
2020-05-08 23:26:55 +0300

    disables HIBERNATE_SEQUENCE w/ generator strategy

--- src/main/java/com/backbone/core/demo/Product.java
+++ src/main/java/com/backbone/core/demo/Product.java
@@ -15,7 +15,7 @@ import javax.persistence.Id;
 public class Product extends RepresentationModel {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // disables HIBERNATE_SEQUENCE
     private int id;
 
     private String category;
