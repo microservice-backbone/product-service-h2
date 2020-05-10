ead47a75a51386b9bd49f3dcd6f34b5aeb418a82
tansudasli <tansudasli@gmail.com>
2020-05-10 14:57:40 +0300

    comments updated

--- src/main/java/com/backbone/core/demo/ProductRepository.java
+++ src/main/java/com/backbone/core/demo/ProductRepository.java
@@ -12,25 +12,35 @@ public interface ProductRepository extends JpaRepository<Product, Integer> {
     Optional<Product> findById(int id);
     Optional<List<Product>> findByCategory(String category);
 
-    /**
-     *  if you need paging and sorting, in custom methods
-     * Optional<List<Product>> findByCategory(String category, Pageable pageable);
-     *
-     */
 
     @Query("select distinct p.category from Product p")
     Optional<List<String>> getDistinctCategories();
 
     /**
-     * JPQL queries, if you need
+     * if you need paging and sorting, in custom methods, and
+     * call w/ PageRequest(page, size, Sort)
      *
-     * @Query("select p from Product p")
-     * List<Product> retrieveAllProducts();
+     * Optional<List<Product>> findByCategory(String category, Pageable pageable);
+     */
+
+    /**
+     * JPQL queries, if you need
+     * - works on JavaObjects,
+     * - suitable for basic CRUD statements
+     * - not suitable for DDL statements
+     *
+     * @Query("select p from Product p where id=:id")
+     * List<Product> retrieveAllProducts(@Param("id" int id));
      */
 
     /**
      * Native (SQL) queries, if you need
+     * - works on Db Tables,
+     * - suitable for complex CRUD statements
+     * - suitable for DDL statements
      *
+     * @Query(value = "select * from Product p where id=:id", nativeQuery=true)
+     * List<Product> retrieveAllProducts(@Param("id" int id));
      *
      */
 
