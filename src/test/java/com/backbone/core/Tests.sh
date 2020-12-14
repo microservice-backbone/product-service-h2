3ec64abbc0db5de5dd98c9943fecfdab094f5965
tansudasli <tansudasli@gmail.com>
2020-12-14 14:56:10 +0300

    Tests scripts merged

--- src/test/java/com/backbone/core/Tests.sh
+++ src/test/java/com/backbone/core/Tests.sh
@@ -1,3 +1,23 @@
+# read requests
+
+echo "get hostname (meaningful in a server)"
+curl localhost:8083/dummy
+
+echo ""
+curl localhost:8083/dummy/Tansu
+
+#read
+printf "\n review/1 >>"
+curl localhost:8083/product/1
+
+printf "\n review/101 : >>"
+curl localhost:8083/product/101
+
+curl localhost:8083/products
+curl localhost:8083/products/page/0/size/1
+
+
+#save
 #create product
 curl -d '{"category":"Book","title":"Lorem ipsum dolor sit amet,",
            "subTitle":"10% discount at cart","brand":"Oreilly","rating":4,
@@ -21,3 +41,9 @@ curl -H 'Content-Type: application/json' \
      -X PUT http://localhost:8083/product
 
 curl localhost:8083/product/10
+
+# delete product
+
+curl -H 'Content-Type: application/json' -X DELETE http://localhost:8083/product/100
+
+curl localhost:8083/product/100
