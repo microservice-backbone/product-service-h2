e8259e17b3f5f4540f02635779ed70ebbbf2f2b9
tansudasli <tansudasli@gmail.com>
2020-12-14 15:00:07 +0300

    Initialize - persistence (repositoty) tests

---
+++ src/test/java/com/backbone/core/PersistenceTests.java
@@ -0,0 +1,12 @@
+package com.backbone.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class PersistenceTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
