282ccb9b6a4810d7c2bf26bc5ace066ac48cc3e0
tansudasli <tansudasli@gmail.com>
2020-12-15 16:11:35 +0300

    Test scripts w/ curl commands

--- src/test/java/com/backbone/core/Tests.sh
+++ src/test/java/com/backbone/core/Tests.sh
@@ -7,7 +7,9 @@ echo ""
 curl localhost:8083/dummy/Tansu
 
 #read
-printf "\n review/1 >>"
+#gets product and calls review service !
+#so run review service, too
+printf "\n product/1 >>"
 curl localhost:8083/product/1
 
 printf "\n review/101 : >>"
@@ -16,6 +18,8 @@ curl localhost:8083/product/101
 curl localhost:8083/products
 curl localhost:8083/products/page/0/size/1
 
+curl localhost:8083/products/page/0/size/100
+
 
 #save
 #create product
@@ -23,7 +27,7 @@ curl -d '{"category":"Book","title":"Lorem ipsum dolor sit amet,",
            "subTitle":"10% discount at cart","brand":"Oreilly","rating":4,
            "shortDescription":"Lorem ipsum dolor","description":"BOOK of ZLITORIS"}' \
       -H 'Content-Type: application/json' \
-      -X PUT http://localhost:8083/product
+      -X POST http://localhost:8083/product
 
 curl localhost:8083/product/101
 
@@ -35,8 +39,9 @@ curl -d '{"id":10,"category":"Fashion","title":"Lorem",
           "shortDescription":"Lorem ipsum dolor sit amet,",
           "description":"ZLITORIS KOMINOTORIS"}' \
      -H 'Content-Type: application/json' \
-     -X PUT http://localhost:8083/product
+     -X POST http://localhost:8083/product
 
+#create product without request body
 curl -H 'Content-Type: application/json' \
      -X PUT http://localhost:8083/product
 
